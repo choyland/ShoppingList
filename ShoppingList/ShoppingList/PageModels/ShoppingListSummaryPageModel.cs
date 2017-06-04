5b0346eeb8cb1413f4a4bc32b366d2f10a06df6c
CHOYLAND-DESKTO\charl <charliehoyland1@gmail.com>
2017-06-04 15:22:28 +0100

    Initial commit

---
+++ ShoppingList/ShoppingList/PageModels/ShoppingListSummaryPageModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreshMvvm;
+using MyShoppingList.Model.Entities;
+
+namespace MyShoppingList.PageModels
+{
+    public class ShoppingListSummaryPageModel : FreshBasePageModel
+    {
+        public List<ShoppingList.Model.Entities.ShoppingList> ShoppingLists { get; set; }
+
+        public ShoppingListSummaryPageModel()
+        {
+            
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+
+            ShoppingLists = new List<ShoppingList>
+            {
+                new ShoppingList {Name = "Test Shopping List", FromDate = DateTime.Now.AddDays(-7), ToDate = DateTime.Now.AddDays(7)},
+                new ShoppingList {Name = "Test Shopping List 2", FromDate = DateTime.Now.AddDays(-14), ToDate = DateTime.Now.AddDays(4)}
+            };
+        }
+    }
+}
